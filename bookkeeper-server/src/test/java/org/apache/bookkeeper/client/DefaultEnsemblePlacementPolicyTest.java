efda68efe94de1f466cfda35ddf7e29359acf272
Alessandro Lioi <lioialessandro2000@live.com>
2023-07-03 16:58:48 +0200

    feat(bookkeeper-server test): add dummy test for configuration testing

--- bookkeeper-server/src/test/java/org/apache/bookkeeper/client/DefaultEnsemblePlacementPolicyTest.java
+++ bookkeeper-server/src/test/java/org/apache/bookkeeper/client/DefaultEnsemblePlacementPolicyTest.java
@@ -6,13 +6,13 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.Parameterized;
 
+import java.awt.print.Book;
 import java.util.*;
 
 // TODO assert also on getAdheringToPolicy
 @RunWith(Parameterized.class)
 public class DefaultEnsemblePlacementPolicyTest {
-    private final List<BookieId> expectedResult;
-    private final EnsemblePlacementPolicy.PlacementPolicyAdherence expectedPlacement;
+    private final EnsemblePlacementPolicy.PlacementResult<List<BookieId>> expected;
     private final int ensembleSize;
     private final int quorumSize;
     private final int ackQuorumSize;
@@ -20,20 +20,17 @@ public class DefaultEnsemblePlacementPolicyTest {
     private final Set<BookieId> excludeBookies;
 
     private static class NewEnsembleParameter {
-        private final List<BookieId> expectedResult;
-        private final EnsemblePlacementPolicy.PlacementPolicyAdherence expectedPlacement;
+        private final EnsemblePlacementPolicy.PlacementResult<List<BookieId>> expected;
         private final int ensembleSize;
         private final int quorumSize;
         private final int ackQuorumSize;
         private final Map<String, byte[]> customMetadata;
         private final Set<BookieId> excludeBookies;
 
-        private NewEnsembleParameter(List<BookieId> expectedResult, EnsemblePlacementPolicy.PlacementPolicyAdherence expectedPlacement,
+        private NewEnsembleParameter(EnsemblePlacementPolicy.PlacementResult<List<BookieId>> expected,
                                      int ensembleSize, int quorumSize, int ackQuorumSize,
                                      Map<String, byte[]> customMetadata, Set<BookieId> excludeBookies) {
-            this.expectedResult = expectedResult;
-            this.expectedPlacement = expectedPlacement;
-
+            this.expected = expected;
             this.ensembleSize = ensembleSize;
             this.quorumSize = quorumSize;
             this.ackQuorumSize = ackQuorumSize;
@@ -45,15 +42,18 @@ public class DefaultEnsemblePlacementPolicyTest {
     @Parameterized.Parameters
     public static Collection<NewEnsembleParameter[]> getParameters() {
         return Arrays.asList(new NewEnsembleParameter[][]{
-                {new NewEnsembleParameter(new ArrayList<>(), EnsemblePlacementPolicy.PlacementPolicyAdherence.FAIL,
+                {new NewEnsembleParameter(
+                        EnsemblePlacementPolicy.PlacementResult.of(
+                                new ArrayList<>(),
+                                EnsemblePlacementPolicy.PlacementPolicyAdherence.FAIL
+                        ),
                         0, 0, 0,
                         new HashMap<>(), new HashSet<>())},
         });
     }
 
     public DefaultEnsemblePlacementPolicyTest(NewEnsembleParameter parameters) {
-        this.expectedResult = parameters.expectedResult;
-        this.expectedPlacement = parameters.expectedPlacement;
+        this.expected = parameters.expected;
 
         this.ensembleSize = parameters.ensembleSize;
         this.quorumSize = parameters.quorumSize;
@@ -66,8 +66,11 @@ public class DefaultEnsemblePlacementPolicyTest {
     public void newEnsembleTest() {
         DefaultEnsemblePlacementPolicy policy = new DefaultEnsemblePlacementPolicy();
         try {
-            EnsemblePlacementPolicy.PlacementResult<List<BookieId>> result = policy.newEnsemble(this.ensembleSize, this.quorumSize, this.ackQuorumSize, this.customMetadata, this.excludeBookies);
-            Assert.assertEquals(this.expectedPlacement, result.getAdheringToPolicy());
+            EnsemblePlacementPolicy.PlacementResult<List<BookieId>> result = policy.newEnsemble(
+                    this.ensembleSize, this.quorumSize, this.ackQuorumSize,
+                    this.customMetadata, this.excludeBookies
+            );
+            Assert.assertEquals(this.expected.getAdheringToPolicy(), result.getAdheringToPolicy());
         } catch (BKException.BKNotEnoughBookiesException e) {
             throw new RuntimeException(e);
         }
