8f3365766f3856e28116ce7af4cf0f8780c32d88
Alessandro Lioi <lioialessandro2000@live.com>
2023-11-10 18:06:17 +0100

    test(DefaultEnsemblePlacementePolicy): tests for `replaceBookie`

--- bookkeeper-server/src/test/java/org/apache/bookkeeper/client/DefaultEnsemblePlacementPolicyTest.java
+++ bookkeeper-server/src/test/java/org/apache/bookkeeper/client/DefaultEnsemblePlacementPolicyTest.java
@@ -213,4 +213,118 @@ public class DefaultEnsemblePlacementPolicyTest {
             }
         }
     }
+
+    @RunWith(Parameterized.class)
+    public static class ReplaceBookieTest {
+        @Parameterized.Parameters
+        public static Collection<Object[]> getParameters() {
+            Map<String, byte[]> emptyMap = new HashMap<>();
+            Map<String, byte[]> validMap = new HashMap<>();
+            validMap.put("string", new byte[]{1});
+            BookieId validId = BookieId.parse("w");
+            BookieId invalidId = BookieId.parse("ro");
+            Set<BookieId> emptySet = new HashSet<>();
+            Set<BookieId> validSet = new HashSet<>();
+            validSet.add(validId);
+            Set<BookieId> invalidSet = Collections.singleton(invalidId);
+            Set<BookieId> mixSet = new HashSet<>();
+            mixSet.add(validId);
+            mixSet.add(invalidId);
+            List<BookieId> emptyList = new ArrayList<>();
+            List<BookieId> validList = Collections.singletonList(validId);
+            List<BookieId> invalidList = Collections.singletonList(invalidId);
+            ExpectedResult<PlacementResult<BookieId>> exception = new ExpectedResult<>(null, Exception.class);
+            ExpectedResult<PlacementResult<BookieId>> valid = new ExpectedResult<>(
+                    PlacementResult.of(BookieId.parse("w2"), PlacementPolicyAdherence.MEETS_STRICT),
+                    null);
+            return Arrays.asList(
+                    new Object[][]{
+                            // e  q  a customMetadata currentEnsemble bookieToReplace excludeBookies expected
+                            {-1, -2, -3, null, null, null, null, exception},
+                            {-1, -2, -2, emptyMap, emptyList, validId, emptySet, exception},
+                            {-1, -2, -1, validMap, validList, invalidId, validSet, exception},
+                            {-1, -1, -2, null, invalidList, null, invalidSet, exception},
+                            {-1, -1, -1, emptyMap, null, validId, mixSet, exception},
+                            {-1, -1, 0, validMap, emptyList, invalidId, null, exception},
+                            {-1, 0, -1, null, validList, null, emptySet, exception},
+                            {-1, 0, 0, emptyMap, invalidList, validId, validSet, exception},
+                            {-1, 0, 1, validMap, null, invalidId, invalidSet, exception},
+                            {0, -1, -2, null, emptyList, null, mixSet, exception},
+                            {0, -1, -1, emptyMap, validList, validId, null, exception},
+//                            {0, -1, 0, validMap, invalidList, invalidId, emptySet, exception}, // Fail
+                            {0, 0, -1, null, null, null, validSet, exception},
+                            {0, 0, 0, emptyMap, emptyList, validId, invalidSet, exception},
+                            {0, 0, 1, validMap, validList, invalidId, mixSet, exception},
+                            {0, 1, 0, null, invalidList, null, null, exception},
+                            {0, 1, 1, emptyMap, null, validId, emptySet, exception},
+                            {0, 1, 2, validMap, emptyList, invalidId, validSet, exception},
+                            {1, 0, -1, null, validList, null, invalidSet, exception},
+                            {1, 0, 0, emptyMap, invalidList, validId, mixSet, exception},
+                            {1, 0, 1, validMap, null, invalidId, null, exception},
+                            {1, 1, 0, null, emptyList, null, emptySet, exception},
+//                            {1, 1, 1, emptyMap, validList, validId, validSet, valid}, // Fail
+                            {1, 1, 2, validMap, invalidList, invalidId, invalidSet, exception},
+                            {1, 2, 1, null, null, null, mixSet, exception},
+                            {1, 2, 2, emptyMap, emptyList, validId, null, exception},
+                            {1, 2, 3, validMap, validList, invalidId, emptySet, exception},
+                            // Improvements
+                            {1, 1, 1, null, validList, validId, emptySet, valid}
+                    }
+            );
+        }
+
+        private DefaultEnsemblePlacementPolicy policy;
+        private final int ensembleSize;
+        private final int writeQuorumSize;
+        private final int ackQuorumSize;
+        private final Map<String, byte[]> customMetadata;
+        private final List<BookieId> currentEnsemble;
+        private final BookieId bookieToReplace;
+        private final Set<BookieId> excludeBookies;
+        private final ExpectedResult<PlacementResult<BookieId>> expected;
+
+        public ReplaceBookieTest(int ensembleSize, int writeQuorumSize, int ackQuorumSize,
+                                 Map<String, byte[]> customMetadata, List<BookieId> currentEnsemble,
+                                 BookieId bookieToReplace, Set<BookieId> excludeBookies,
+                                 ExpectedResult<PlacementResult<BookieId>> expected) {
+            this.ensembleSize = ensembleSize;
+            this.writeQuorumSize = writeQuorumSize;
+            this.ackQuorumSize = ackQuorumSize;
+            this.customMetadata = customMetadata;
+            this.currentEnsemble = currentEnsemble;
+            this.bookieToReplace = bookieToReplace;
+            this.excludeBookies = excludeBookies;
+            this.expected = expected;
+        }
+
+        @Before
+        public void setup() {
+            ClientConfiguration conf = TestBKConfiguration.newClientConfiguration();
+            conf.setDiskWeightBasedPlacementEnabled(true);
+            policy = (DefaultEnsemblePlacementPolicy) new DefaultEnsemblePlacementPolicy()
+                    .initialize(conf, Optional.empty(), null, DISABLE_ALL, NullStatsLogger.INSTANCE, null);
+            Set<BookieId> initialWritable = new HashSet<>();
+            initialWritable.add(BookieId.parse("w"));
+            initialWritable.add(BookieId.parse("w2"));
+            policy.onClusterChanged(initialWritable, new HashSet<>());
+        }
+
+        @Test
+        public void replaceBookieTest() {
+            try {
+                PlacementResult<BookieId> result = policy.replaceBookie(ensembleSize, writeQuorumSize, ackQuorumSize,
+                        customMetadata, currentEnsemble, bookieToReplace, excludeBookies);
+                Assert.assertNull(expected.getException());
+                Assert.assertEquals(expected.getT().getResult().getId(), result.getResult().getId());
+                Assert.assertEquals(expected.getT().getAdheringToPolicy(), result.getAdheringToPolicy());
+            } catch (Exception e) {
+                Assert.assertNotNull(expected.getException());
+            }
+        }
+
+        @After
+        public void teardown() {
+            policy.uninitalize();
+        }
+    }
 }
